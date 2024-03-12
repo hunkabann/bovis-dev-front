8946b55055efe7cdaa33c20301d916b66c5fdc7c
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-03-12 13:37:23 -0600

    Update progreso.component.ts Style rojo < 100 y Verde = 100

--- src/app/auditoria/components/progreso/progreso.component.ts
+++ src/app/auditoria/components/progreso/progreso.component.ts
@@ -10,12 +10,20 @@ export class ProgresoComponent implements OnInit {
   @Input() totalDocumentos: number = 0;
   @Input() totalDocumentosValidados: number = 0;
 
+  styleBarra: string
+
   constructor() { }
 
   get total() {
 
     if(this.totalDocumentos == 0 && this.totalDocumentosValidados == 0) {
       return 0
+    }
+
+    if(this.totalDocumentos == this.totalDocumentosValidados) {
+      this.styleBarra = "customProgressCompleto"
+    }else{
+      this.styleBarra = "customProgress"
     }
     
     return Math.round((this.totalDocumentosValidados / this.totalDocumentos) * 100)
