e38f4743d903ea237781b78f887205040ff92e95
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-04-02 21:04:45 -0600

    Update proyecto-join.pipe.ts agregar metodo para calcular y pintar en rojo o en blanco

--- src/app/timesheet/pipes/proyecto-join.pipe.ts
+++ src/app/timesheet/pipes/proyecto-join.pipe.ts
@@ -15,6 +15,37 @@ export class ProyectoJoinPipe implements PipeTransform {
       case 'proyectos':
         // proyectos.forEach(proyecto => total += proyecto.tDedicacion)
         proyectos.forEach(proyecto => total += proyecto.dias)
+        total = this.formateaValor((total / +totalDias) * 100)
+        break
+      case 'proyectosDias':
+        proyectos.forEach(proyecto => total += proyecto.dias)
+        break
+      case 'otros':
+        // proyectos.forEach(otro => total += otro.tDedicacion)
+        proyectos.forEach(otro => total += otro.dias)
+        total = this.formateaValor((total / +totalDias) * 100)
+        break
+      case 'otrosDias':
+        proyectos.forEach(otro => total += otro.dias)
+        break
+      default:
+        total = 0
+    }
+
+    return total
+    // const arr = proyectos.map(proyecto => `${proyecto.descripcion}: ${proyecto.tDedicacion} %`)
+    // return arr.join(" | ");
+  }
+
+  transformCalcula(proyectos: Otro[] | Proyecto[], ...args: unknown[]): number {
+
+    const [tipo, totalDias] = args
+    let total = 0
+
+    switch(tipo) {
+      case 'proyectos':
+        // proyectos.forEach(proyecto => total += proyecto.tDedicacion)
+        proyectos.forEach(proyecto => total += proyecto.dias)
         total = Math.round((total / +totalDias) * 100)
         break
       case 'proyectosDias':
