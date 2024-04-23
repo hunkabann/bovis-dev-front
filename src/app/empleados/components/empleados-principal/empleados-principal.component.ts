97072fe619d1dea84732d9d4c358645fbd378d76
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-04-22 20:04:37 -0600

    Update empleados-principal.component.ts Excel

--- src/app/empleados/components/empleados-principal/empleados-principal.component.ts
+++ src/app/empleados/components/empleados-principal/empleados-principal.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import { EmpleadosService } from '../../services/empleados.service';
-import { CatEmpleado, UpEmpleado,Empresas, Proyectos,Busqueda } from '../../Models/empleados';
+import { CatEmpleado, UpEmpleado,Empresas, Proyectos,Busqueda,encabezados } from '../../Models/empleados';
 import { SharedService } from 'src/app/shared/services/shared.service';
 import { finalize, forkJoin } from 'rxjs';
 import { MessageService } from 'primeng/api';
-import { SUBJECTS, TITLES } from 'src/utils/constants';
+import { SUBJECTS, TITLES,EXCEL_EXTENSION } from 'src/utils/constants';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Item,Opcion } from 'src/models/general.model';
@@ -12,6 +12,11 @@ import { Table } from 'primeng/table';
 import { DialogService } from 'primeng/dynamicdialog';
 import { MostrarProyectosComponent } from '../mostrar-proyectos/mostrar-proyectos.component';
 import { Dropdown } from 'primeng/dropdown';
+import * as XLSX from 'xlsx';
+import { formatCurrency } from 'src/helpers/helpers';
+import * as ExcelJS from 'exceljs';
+import { saveAs } from 'file-saver';
+import { DatePipe } from '@angular/common';
 
 interface FiltroCancelacion {
   name: string;
@@ -33,7 +38,11 @@ export class EmpleadosPrincipalComponent implements OnInit {
     {label: 'Inactivo', value: false}
   ]
 
+  today: Date = new Date();
+  pipe = new DatePipe('en-US');
+  todayWithPipe = null;
 
+  //allData: CieRegistro[] = []
 
   filtroProyectos: FiltroCancelacion[] = [];
   filtroEmpresas: FiltroCancelacion[] = [];
@@ -253,6 +262,93 @@ export class EmpleadosPrincipalComponent implements OnInit {
     this.opcionFiltro = 0;
   }
 
+  exportJsonToExcel(): void {
+
+    const workbook = new ExcelJS.Workbook()
+
+    const worksheet = workbook.addWorksheet('Detalle')
+
+    // Tìtulos
+    this._setXLSXTitles(worksheet)
+
+    // Encabezados
+    this._setXLSXHeader(worksheet)
+
+    let row = 5
+    // Contenido
+    row = this._setXLSXContent(worksheet, row)
+
+    // Contenido
+    //this._setXLSXContent(worksheet)
+
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' })
+
+      //saveAs(blob, `FacturacionCancelacion_${Date.now()}${EXCEL_EXTENSION}`)
+      this.todayWithPipe = this.pipe.transform(Date.now(), 'dd_MM_yyyy');
+
+      saveAs(blob, `Empleados_` + this.todayWithPipe + `${EXCEL_EXTENSION}`)
+    });
+  }
+
+  _setXLSXTitles(worksheet: ExcelJS.Worksheet) {
+  }
+
+  _setXLSXHeader(worksheet: ExcelJS.Worksheet) {
+    const fill: ExcelJS.Fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'FF4681CB' } }
+    const alignment: Partial<ExcelJS.Alignment> = { vertical: 'middle', horizontal: 'center', wrapText: true }
+
+    encabezados.forEach((encabezado, index) => {
+      let cell = worksheet.getCell(4, index + 1)
+      cell.value = encabezado.label
+      cell.fill = fill
+      cell.alignment = alignment
+    })
+  }
+
+  _setXLSXContent(worksheet: ExcelJS.Worksheet, row: number): number {
+
+    const fillCancelada: ExcelJS.Fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'ea899a' } }
+    const fillFactura: ExcelJS.Fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'ffffff' } }
+
+    this.empleados.forEach(record => {
+      //worksheet.getCell(row).fill = record.fechaCancelacion ? fillCancelada : fillFactura
+      //const col = row.getCell(row);
+
+      worksheet.getCell(row, 1).value = record.nunum_empleado_rr_hh
+      worksheet.getCell(row, 2).value = record.nombre_persona
+      worksheet.getCell(row, 3).value = record.chtipo_emplado
+      worksheet.getCell(row, 4).value = record.chcategoria
+      worksheet.getCell(row, 5).value = record.chtipo_contrato
+     
+        worksheet.getCell(row, 6).value = record.chpuesto
+     
+
+      worksheet.getCell(row, 7).value = record.chempresa
+      worksheet.getCell(row, 8).value = record.chciudad
+      worksheet.getCell(row, 9).value = record.nukidjefe_directo
+      worksheet.getCell(row, 10).value = record.chunidad_negocio
+      worksheet.getCell(row, 11).value = record.dtfecha_ingreso
+      worksheet.getCell(row, 12).value = record.dtfecha_salida
+      worksheet.getCell(row, 13).value = record.dtfecha_ultimo_reingreso
+      worksheet.getCell(row, 14).value = record.chnss
+      worksheet.getCell(row, 15).value = record.chemail_bovis
+      worksheet.getCell(row, 16).value = record.nuantiguedad
+      worksheet.getCell(row, 17).value = record.nufondo_fijo
+      //worksheet.getCell(row, 18).value = record.responsable
+      //worksheet.getCell(row, 19).value = record.tipoProyecto
+      //worksheet.getCell(row, 20).value = record.tipoPy
+      //worksheet.getCell(row, 21).value = record.clasificacionPy
+      //worksheet.getCell(row).fill = {  type: 'pattern', pattern: 'solid', fgColor: { argb: record.fechaCancelacion ?  'FFC000':  '70AD47'}};
+      //worksheet.getRow(row).fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: record.fechaCancelacion ? 'ea899a' : 'ffffff' } };
+      row++
+    });
+
+    return row
+
+  }
+  
+  
   
 
  
