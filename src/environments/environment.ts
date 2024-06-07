3e7526360f2cd6200f59775668667e53533efb4f
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-06-06 19:36:23 -0600

    Update environment.ts cambio a purple

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,23 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+/*
 export const environment = {
   production: false,
   urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
   redirectUri: 'http://localhost:4200/',
   clientID: '2a3addef-64ef-407c-a86a-b9913237e5c5',
+  urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3',
+  tokenUser: 'B0V1$-2023',
+  tokenSecret: 'P4$w0RD_2023'
+};
+ */
+
+export const environment = {
+  production: true,
+  urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
+  redirectUri: 'https://purple-grass-0a7dafc0f.2.azurestaticapps.net/',
+  clientID: '2a3addef-64ef-407c-a86a-b9913237e5c5',  
   urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3',
   tokenUser: 'B0V1$-2023',
   tokenSecret: 'P4$w0RD_2023'
