3cad63bceb56ddf6250935dc3caaae8e9a79ae42
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-06-06 21:30:23 -0600

    Update environment.prod.ts redirect a purple

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,7 +12,7 @@
 export const environment = {
   production: false,
   urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
-  redirectUri: 'http://localhost:4200/',
+  redirectUri: 'https://purple-grass-0a7dafc0f.2.azurestaticapps.net/',
   clientID: '2a3addef-64ef-407c-a86a-b9913237e5c5',
   urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3',
   tokenUser: 'B0V1$-2023',
