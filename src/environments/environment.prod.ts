d8c3ecbf08d65385fca8e5d8fff7ceb6610b001f
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-06-06 21:22:17 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,20 @@
-export const environment = {
+/*export const environment = {
   production: true,
   urlApiBovis: 'https://web-api-bovis.azurewebsites.net/',
   //redirectUri: 'https://zealous-bush-0a2ce3c0f.4.azurestaticapps.net/',
   redirectUri: 'https://proud-sea-0d98bd40f.2.azurestaticapps.net/',
   clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11', 
   urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f',
+  tokenUser: 'B0V1$-2023',
+  tokenSecret: 'P4$w0RD_2023'
+};*/
+
+export const environment = {
+  production: false,
+  urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
+  redirectUri: 'http://localhost:4200/',
+  clientID: '2a3addef-64ef-407c-a86a-b9913237e5c5',
+  urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3',
   tokenUser: 'B0V1$-2023',
   tokenSecret: 'P4$w0RD_2023'
 };
