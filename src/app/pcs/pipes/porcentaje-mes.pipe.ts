d73583909e8e3a9855bb7446c108c2e3254b45a5
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-07-30 19:00:41 -0600

    Update porcentaje-mes.pipe.ts formato subtotal en moneda

--- src/app/pcs/pipes/porcentaje-mes.pipe.ts
+++ src/app/pcs/pipes/porcentaje-mes.pipe.ts
@@ -1,6 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Fecha } from '../models/pcs.model';
 import { Mes } from 'src/models/general.model';
+import {formatCurrency, getCurrencySymbol} from '@angular/common';
 
 @Pipe({
   name: 'porcentajeMes'
@@ -23,14 +24,16 @@ export class PorcentajeMesPipe implements PipeTransform {
       if(mes && anio && mes.porcentaje > 0) {
 
         //console.log(' mes.porcentaje ------>> ' + mes.porcentaje)
-        return mes.porcentaje
+        return this.formateaValor(formatCurrency(mes.porcentaje, 'en-US', getCurrencySymbol('USD', 'wide')))
       }
-
-   
-
-    
 
     return '-';
   }
+  
+  formateaValor(valor) {
+    // si no es un número devuelve el valor, o lo convierte a número con 4 decimales
+    return isNaN(valor) ? valor : parseFloat(valor).toFixed(2);
+  }
+
 
 }
