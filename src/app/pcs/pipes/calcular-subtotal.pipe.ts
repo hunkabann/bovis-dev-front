5f239d7ca23b84dc593222921da94fe55bb5fbb9
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-07-30 19:02:02 -0600

    Update calcular-subtotal.pipe.ts formato subtotales a moneda

--- src/app/pcs/pipes/calcular-subtotal.pipe.ts
+++ src/app/pcs/pipes/calcular-subtotal.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Fecha } from '../models/pcs.model';
+import {formatCurrency, getCurrencySymbol} from '@angular/common';
 
 @Pipe({
   name: 'calcularSubtotal'
@@ -7,6 +8,7 @@ import { Fecha } from '../models/pcs.model';
 export class CalcularSubtotalPipe implements PipeTransform {
 
   transform(value: Fecha[], ...args: unknown[]): unknown {
+    console.log(Object.values(value));
     
     let subtotal = 0
 
@@ -14,7 +16,15 @@ export class CalcularSubtotalPipe implements PipeTransform {
       subtotal += +fecha.porcentaje
     })
 
-    return subtotal
+    return this.formateaValor(formatCurrency(subtotal, 'en-US', getCurrencySymbol('USD', 'wide')))
+  }
+
+
+  formateaValor(valor) {
+    // si no es un número devuelve el valor, o lo convierte a número con 4 decimales
+    return isNaN(valor) ? valor : parseFloat(valor).toFixed(2);
   }
 
 }
+
+
