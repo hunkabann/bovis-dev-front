582d46a36c82fa8d8006d04561b53fd38fc0b43b
elbaltezar <norberto.baltezar@gmail.com>
2024-08-08 22:10:54 -0600

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-// LOCAL s
+// LOCAL
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
