15ba91a8765bb724b47e0a2558d730913fb2c874
elbaltezar <norberto.baltezar@gmail.com>
2024-08-08 22:11:24 -0600

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
