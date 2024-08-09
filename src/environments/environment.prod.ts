55a3020c887007bf159fa21ecea696e90478e799
elbaltezar <norberto.baltezar@gmail.com>
2024-08-08 21:51:17 -0600

    Update Prod environment to Main

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,16 @@
-/*export const environment = {
-  production: true,
-  urlApiBovis: 'https://web-api-bovis.azurewebsites.net/',
-  //redirectUri: 'https://zealous-bush-0a2ce3c0f.4.azurestaticapps.net/',
-  redirectUri: 'https://proud-sea-0d98bd40f.2.azurestaticapps.net/',
-  clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11', 
-  urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f',
-  tokenUser: 'B0V1$-2023',
-  tokenSecret: 'P4$w0RD_2023'
-};*/
+// PRODUCTION
+// export const environment = {
+//   production: true,
+//   urlApiBovis: 'https://web-api-bovis.azurewebsites.net/',
+//   //redirectUri: 'https://zealous-bush-0a2ce3c0f.4.azurestaticapps.net/',
+//   redirectUri: 'https://proud-sea-0d98bd40f.2.azurestaticapps.net/',
+//   clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11', 
+//   urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f',
+//   tokenUser: 'B0V1$-2023',
+//   tokenSecret: 'P4$w0RD_2023'
+// };
 
+// DEV
 export const environment = {
   production: false,
   urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
@@ -18,4 +20,3 @@ export const environment = {
   tokenUser: 'B0V1$-2023',
   tokenSecret: 'P4$w0RD_2023'
 };
-// 2
