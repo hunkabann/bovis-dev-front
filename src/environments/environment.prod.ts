5d1acec2c73c43a4639c18c8f7ab9b69127feec0
elbaltezar <norberto.baltezar@gmail.com>
2024-08-08 21:49:23 -0600

    Update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
-/*export const environment = {
+// PRODUCTION
+export const environment = {
   production: true,
   urlApiBovis: 'https://web-api-bovis.azurewebsites.net/',
   //redirectUri: 'https://zealous-bush-0a2ce3c0f.4.azurestaticapps.net/',
@@ -7,15 +8,4 @@
   urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f',
   tokenUser: 'B0V1$-2023',
   tokenSecret: 'P4$w0RD_2023'
-};*/
-
-export const environment = {
-  production: false,
-  urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
-  redirectUri: 'https://purple-grass-0a7dafc0f.2.azurestaticapps.net/',
-  clientID: '2a3addef-64ef-407c-a86a-b9913237e5c5',
-  urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3',
-  tokenUser: 'B0V1$-2023',
-  tokenSecret: 'P4$w0RD_2023'
 };
-// 2
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
+// PURPLE
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
